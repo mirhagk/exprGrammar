af950095e46e4227f7ca9ae4e05b9033e88367aa
Sadisha Galappatti <sadishag@gmail.com>
2014-07-29 22:20:46 -0400

    Program has the code to compile the parsed text file. Compiler needs some work.

--- exprGrammar/exprGrammar/Program.cs
+++ exprGrammar/exprGrammar/Program.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace exprGrammar
 {
     class Program
     {
+        string parserFile = "./exprGrammar";
+        string testCode = "./exprGrammar.txt";
+
         static void Main(string[] args)
         {
             var grammar = new Grammar();
             grammar.Parse("");
+
+            
+            Console.WriteLine(Compiler.Compile(grammar.parse(File.ReadAllText(testCode))));
+
         }
     }
 }
